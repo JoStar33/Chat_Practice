bb3c68de0400c856c126ba871cae4e8f6cda3f11
[아이엔소프트] 황호세 <hshwang@in-soft.co.kr>
2023-02-13 13:47:59 +0900

    chore: 버튼 구현, 주석 수정

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ const io = SocketIO(server, {path: '/socket.io'});
 io.on('connection', function (socket) {
   console.log(socket.id, ' connected...');
   
-  // broadcasting a entering message to everyone who is in the chatroom
   io.emit('msg', `${socket.id} has entered the chatroom.`);
 
   // message receives
   socket.on('msg', function (data) {
       console.log(socket.id,': ', data);
-      // broadcasting a message to everyone except for the sender
+      // sender인 socket의 클라이언트는 제외한다.
       socket.broadcast.emit('msg', `${socket.id}: ${data}`);
   });
 
